fbd91466eda9daf76e7a100edcd32b190ff4813e
albinmmathew <albinmmathew@gmail.com>
2026-05-29 00:07:12 +0530

    -

---
+++ 389.py
@@ -0,0 +1,10 @@
+# Find the Difference
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        res = 0
+        for i in s:
+            res^=ord(i)
+        for i in t:
+            res^=ord(i)
+        return chr(res)
