8b854254e68b9dad874bb745d570d3df02de46ba
albinmmathew <albinmmathew@gmail.com>
2026-06-24 10:23:58 +0530

    -

---
+++ 1978.sql
@@ -0,0 +1,7 @@
+-- Employees Whose Manager Left the Company
+
+/* Write your PL/SQL query statement below */
+SELECT EMPLOYEE_ID FROM EMPLOYEES 
+    WHERE SALARY<30000 
+    AND MANAGER_ID NOT IN (SELECT EMPLOYEE_ID FROM EMPLOYEES)
+    ORDER BY EMPLOYEE_ID;
